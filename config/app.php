11aab317814a699c9975688ead9b30c2be43dcdf
thole3287 <thole17062002@gmail.com>
2023-03-13 11:53:02 +0700

    BakeryStore Project

--- config/app.php
+++ config/app.php
@@ -1,5 +1,5 @@
 <?php
-
+// use JWTAuth;
 use Illuminate\Support\Facades\Facade;
 
 return [
@@ -181,6 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
@@ -210,6 +212,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ])->toArray(),
 
 ];
