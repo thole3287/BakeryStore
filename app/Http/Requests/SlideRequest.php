95610278b52fadebeba6fcb7d56a5d14de68b64c
thole3287 <thole17062002@gmail.com>
2023-04-26 20:32:35 +0700

    slide rules

--- app/Http/Requests/SlideRequest.php
+++ app/Http/Requests/SlideRequest.php
@@ -21,33 +21,17 @@ class SlideRequest extends FormRequest
      */
     public function rules(): array
     {
-        if(request()->isMethod('post')) {
-            return [
-                'name' => 'required|string|max:258',
-                // 'image' => 'required|image|mimes:jpeg,png,jpg, webp,gif,svg|max:2048',
-                'image' => 'required'
-            ];
-        } else {
-            return [
-                'name' => 'required|string|max:258',
-                // 'image' => 'nullable|image|mimes:jpeg,png,webp,jpg,gif,svg|max:2048',   
-                'image' => 'nullable'
-  
-            ];
-        }
+        return [
+            'name' => 'nullable|string|max:258',
+            // 'image' => 'nullable|image|mimes:jpeg,png,webp,jpg,gif,svg|max:2048',   
+            'image' => 'required'
+        ];
     }
 
     public function messages()
     {
-        if(request()->isMethod('post')) {
-            return [
-                'name.required' => 'Name is required!',
-                'image.required' => 'Image is required!',
-            ];
-        } else {
-            return [
-                'name.required' => 'Name is required!',
-            ];   
-        }
+        return [
+            'image.required' => 'Image is required!',
+        ];
     }
 }
