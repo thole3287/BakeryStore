5bcb91ab3b9228d3c80dfb322955b0b8a0a0c87e
thole3287 <thole17062002@gmail.com>
2023-05-03 16:07:27 +0700

    change port mail

--- resources/views/emails/passwordResetMail.blade.php
+++ resources/views/emails/passwordResetMail.blade.php
@@ -1,10 +1,10 @@
 <x-mail::message>
-# Reset Password
-To change your password, please click the 'Change Password' button below
-<x-mail::button :url="'http://localhost:81/response-password-reset?token='.$token">
-Change Password
-</x-mail::button>
+    # Reset Password
+    To change your password, please click the 'Change Password' button below
+    <x-mail::button :url="'http://localhost:3000/response-password-reset?token=' . $token">
+        Change Password
+    </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    {{ config('app.name') }}
 </x-mail::message>
