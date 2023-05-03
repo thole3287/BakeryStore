53fb835ad43f7089da9194394d95d21fe9db2ec2
thole3287 <thole17062002@gmail.com>
2023-05-03 16:11:36 +0700

    remove query pagrams token

--- resources/views/emails/passwordResetMail.blade.php
+++ resources/views/emails/passwordResetMail.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
     # Reset Password
     To change your password, please click the 'Change Password' button below
-    <x-mail::button :url="'http://localhost:3000/response-password-reset?token=' . $token">
+    <x-mail::button :url="'http://localhost:3000/response-password-reset' . $token">
         Change Password
     </x-mail::button>
 
